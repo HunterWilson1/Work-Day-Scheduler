5918d3dc705fb3163cc5b3f927dc816200298540
Hunter Wilson <huntertheguy123@gmail.com>
2023-01-30 13:46:13 -0600

    I made the time blocks from 9-5 and added a function using dayjs to keep track of time

--- Assets/script.js
+++ Assets/script.js
@@ -19,5 +19,10 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  
 });
+// TODO: Add code to display the current date in the header of the page.
+setInterval(() => {
+  const dateTime = dayjs().format("MMM D, YYYY hh:mm a");
+  document.getElementById("currentDay").innerHTML = dateTime;
+}, 1000);
