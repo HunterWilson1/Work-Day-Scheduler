19261338ce90d7db81acc1c2d26915e3e3719db7
Hunter Wilson <huntertheguy123@gmail.com>
2023-01-31 20:23:21 -0600

    changed the siblings in the save button function and added the get item for local storage

--- Assets/script.js
+++ Assets/script.js
@@ -50,8 +50,8 @@ $(function () {
   //event handler for the save button so that the text description is saved to local storage.
   $(document).ready(function () {
     saveButton.on("click", function () {
-      //retrives the value of id attribute
-      var time = $(this).siblings().attr('id');
+      //retrives the value of id attribute from the parent element
+      var time = $(this).parent().attr('id');
       //retrieves the text area description 
       var text = $(this).siblings(".description").val();
       //saves variables to local storage
@@ -60,10 +60,15 @@ $(function () {
   });
   tracker();
 
-
+  //loads saved data from the local storage
+  $('hour-9 .doc').val(localStorage.getItem('hour-9'));
+  $('hour-10 .doc').val(localStorage.getItem('hour-10'));
+  $('hour-11 .doc').val(localStorage.getItem('hour-11'));
+  $('hour-12 .doc').val(localStorage.getItem('hour-12'));
+  $('hour-13 .doc').val(localStorage.getItem('hour-13'));
+  $('hour-14 .doc').val(localStorage.getItem('hour-14'));
+  $('hour-15 .doc').val(localStorage.getItem('hour-15'));
+  $('hour-16 .doc').val(localStorage.getItem('hour-16'));
+  $('hour-17 .doc').val(localStorage.getItem('hour-17'));
 });
 // TODO: Add code to display the current date in the header of the page.
-// bleep blop bloop
-function test() {
-  
-}
